05b0ffc369409dc1a7ff47a6d1da8001f0d67d83
Colin Watson <cjwatson@debian.org>
2007-06-13 00:28:26 +0100

    1:4.6p1-1 (patches unapplied)
    Imported using git-ubuntu import.

---
+++ version.h
@@ -0,0 +1,9 @@
+/* $OpenBSD: version.h,v 1.49 2007/03/06 10:13:14 djm Exp $ */
+
+#define SSH_VERSION	"OpenSSH_4.6"
+
+#define SSH_PORTABLE	"p1"
+#ifndef SSH_EXTRAVERSION
+#define SSH_EXTRAVERSION
+#endif
+#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_EXTRAVERSION
